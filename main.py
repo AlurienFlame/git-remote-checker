049a6a34b4a8a994079001e966fdc6d274ec3042
Lucien Lasseau <62194841+AlurienFlame@users.noreply.github.com>
2023-06-02 15:17:24 -0700

    Group projects by problem

--- main.py
+++ main.py
@@ -9,12 +9,21 @@ def main():
         print("No directory given, using default (~/dev)")
         root = "/home/lucien/dev"
 
+    not_repo = []
+    no_remote = []
+
     for dirname in os.listdir(root):
         # run git remote inside each directory
-        os.chdir(os.path.join(root, dirname))
+        full_path = os.path.join(root, dirname)
+
+        if not os.path.isdir(full_path):
+            # Ignore files
+            continue
+
+        os.chdir(full_path)
         if not os.path.exists(".git"):
             # This is not a git repo, we want to hear about it.
-            print(f"{dirname} is not a git repo")
+            not_repo.append(dirname)
             continue
 
         completed_process = sp.run(["git", "remote"], stdout=sp.PIPE)
@@ -23,7 +32,7 @@ def main():
 
         if len(remotes) == 0:
             # This is the case we really want to know about.
-            print(f"{dirname} has no remotes")
+            no_remote.append(dirname)
             continue
         if len(remotes) >= 1:
             # Say nothing, this is the preferred case.
@@ -31,6 +40,14 @@ def main():
 
         # We shouldn't get this far, but if we do we want to know what's going on.
         print(completed_process)
+
+    print("\nThe following projects have not had git repos initialized:")
+    for dirname in not_repo:
+        print(dirname)
+
+    print("\nThe following projects are not backed up to the cloud:")
+    for dirname in no_remote:
+        print(dirname)
 
 
 if __name__ == "__main__":
